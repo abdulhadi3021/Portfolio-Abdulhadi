f382dbae1a481d789b11bc1722585826a8070db5
abdulhadi3021 <getsecondtime@gmail.com>
2025-05-09 10:38:01 +0500

    Remove Vercel SpeedInsights to fix build error
    Deleted import and usage of '@vercel/speed-insights/next' in _app.tsx to resolve module not found error during Vercel deployment.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { SpeedInsights } from '@vercel/speed-insights/next' 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,7 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </main>
       <Footer />
-      <SpeedInsights />
     </div>
   )
 }
